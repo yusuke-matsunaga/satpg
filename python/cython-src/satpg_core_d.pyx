c2a731fdb4ffee4d57b9c0ae995e2e96a0482562
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-06-20 00:10:05 +0900

    python/cython-src/satpg_core_d.pyx を削除

--- python/cython-src/satpg_core_d.pyx
+++
@@ -1,9 +0,0 @@
-#
-# @file satpg.pyx
-# @brief satpg の python インターフェイス
-# @author Yusuke Matsunaga (松永 裕介)
-#
-# Copyright (C) 2016 Yusuke Matsunaga
-# All rights reserved.
-
-include "satpg_core.pyx"
