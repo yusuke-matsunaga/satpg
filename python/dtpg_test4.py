025bb237b04454902842ef1bea4c7da28a6d5889
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-08-04 09:15:23 +0900

    python/dtpg_test4.py: 修正

--- python/dtpg_test4.py
+++ python/dtpg_test4.py
@@ -8,13 +8,52 @@
 # All rights reserved.
 
 import argparse
-#import os.path
-#import time
+import os.path
+import time
+from satpg_core import TpgNetwork
 from satpg_core import FaultType
+from satpg_core import MinPatMgr
+from dtpg import Dtpg
 from exec_one import exec_one
 
 #algorithm_list = ('coloring2', 'mincov+dsatur', 'mincov+isx', 'isx+mincov', 'dsatur+mincov')
 algorithm_list = ( 'coloring2', 'mincov+dsatur' )
+
+def dtpg1(file_name, fault_type) :
+    body, ext = os.path.splitext(file_name)
+    if ext == '.blif' :
+        file_format = 'blif'
+    elif ext == '.bench' :
+        file_format = 'iscas89'
+    else :
+        # デフォルト
+        file_format = 'blif'
+
+    if file_format == 'blif' :
+        network = TpgNetwork.read_blif(file_name)
+    elif file_format == 'iscas89' :
+        network = TpgNetwork.read_iscas89(file_name)
+    else:
+        # ここに来るはずはない．
+        assert False
+
+    if not network :
+        print('Error, could not read {}'.format(file_name))
+        return
+
+    start = time.process_time()
+
+    dtpg = Dtpg(network, fault_type)
+
+    ndet, nunt, nabt = dtpg.ffr_mode(False)
+
+    fault_list = MinPatMgr.fault_reduction(dtpg.fault_list, network, fault_type, 'red1,red2')
+
+    end = time.process_time()
+    cpu_time = end - start
+
+    return dtpg, network, fault_list
+
 
 def main() :
 
@@ -55,8 +94,9 @@ def main() :
 
     k = int(args.k)
     for file_name in args.file_list :
+        dtpg, network, fault_list = dtpg1(file_name, fault_type)
         r_dic = {}
-        exec_one(file_name, fault_type, k, algorithm_list, r_dic, verbose)
+        exec_one(dtpg, network, fault_type, fault_list, k, algorithm_list, r_dic, verbose)
         for sig, ( nv, cpu_time) in r_dic.items() :
             print('{:40}: {:5}: {:8.2f}'.format(sig, nv, cpu_time))
 
