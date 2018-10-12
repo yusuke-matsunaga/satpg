28c4bf01c2f0fb92aa31d87716280c460ea851aa
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-10-12 12:48:44 +0900

    ym-common の変更にともなう CMakeLists.txt の修正
    * doxygen-src/Doxyfile.in を etc/ に移動
    * doxygen-src/CMakeLists.txt を削除
    * doxygen-src/mainpage.h を追加

---
+++ doxygen-src/mainpage.h
@@ -0,0 +1,19 @@
+﻿/// @file mainpage.h
+/// @brief Satpg2 のメインページ
+/// @author Yusuke Matsunaga (松永 裕介)
+///
+/// Copyright (C) 2018 Yusuke Matsunaga
+/// All rights reserved.
+
+
+/// @mainpage
+///
+/// Satpg2 は SAT ソルバを用いたテスト生成プログラムです．
+///
+/// 現在は tclsh の拡張版の atpg_tclsh と Python ライブラリ
+/// の２種類があります．
+///
+/// tclsh 版は今後は開発予定はありません．
+/// すべて Python 版に移行します．
+
+#error "don't include me!"
