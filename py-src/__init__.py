0405b28a03d4bc8cbefa83a887297bc8252c55e1
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2018-10-29 17:24:59 +0900

    py-src 作成中

---
+++ py-src/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+### @file __init__.py
+### @brief satpg パッケージの初期化スクリプト
+### @author Yusuke Matsunaga (松永 裕介)
+###
+### Copyright (C) 2018 Yusuke Matsunaga
+### All rights reserved.
+
+from satpg.satpg_core import DtpgFFR
+from satpg.satpg_core import DtpgMFFC
+from satpg.satpg_core import FaultStatus
+from satpg.satpg_core import FaultType
+from satpg.satpg_core import Fsim
+from satpg.satpg_core import TestVector
+from satpg.satpg_core import TpgNetwork
